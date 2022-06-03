f87eba97b967162c12f2a7d9b2a1ba32e813dfa1
فواز عسيري <Fowazassiri@gmail.com>
2022-06-03 11:13:42 +0300

    first

---
+++ ex2.js
@@ -0,0 +1,13 @@
+//at weekend
+///define objects 
+const emp=[
+{id:1,Name:Mohammed,age:18,level:intermediate,tasksCompleted:8},
+{id:2,Name:Omar,age:22,level:beginner,tasksCompleted:2},
+{id:3,Name:Sarah,age:20,level:beginner,tasksCompleted:4},
+{id:4,Name:Saleh,age:15,level:intermediate,tasksCompleted:6},
+{id:5,Name:Saud,age:30,level:advanced,tasksCompleted:10},
+{id:6,Name:Amani,age:25,level:advanced,tasksCompleted:10},
+{id:7,Name:Lojain,age:23,level:advanced,tasksCompleted:10}]
+function print(){
+    
+}
